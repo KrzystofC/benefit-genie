3fdba8a0f4d6826f2c4288ea952adbc9f3bdcc22
Krzysztof Cackowski <krzysztof.cackowski@aerdata.com>
2024-11-18 13:15:20 +0100

    changed logo variable

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -42,7 +42,7 @@ const Layout = () => {
 
   useEffect(() => {
     if (!appStateContext?.state.isLoading) {
-      setLogo(ui?.logo || Contoso)
+      setLogo(Contoso)
     }
   }, [appStateContext?.state.isLoading])
 
